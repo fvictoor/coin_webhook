d344f1992a52dcb1efa5d76a22ee5cb831265f12
Victor Hugo Porto <fvictoor@outlook.com>
2024-10-08 09:24:22 -0300

    Update final

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ URL_ENDPOINT = config['URL_ENDPOINT']
 API_KEY = config['API_KEY']
 MOEDA_VALOR = config['MOEDA_VALOR']
 MOEDA_BUY = config['MOEDA_BUY']
-
+WEBHOOK_URL = config['WEBHOOK_URL']
 # Construa a URL completa
 url = f"{URL_ENDPOINT}{API_KEY}/latest/{MOEDA_VALOR}"
 
@@ -27,6 +27,12 @@ if response.status_code == 200:
 
     if taxa_cambio:
         print(f" {MOEDA_VALOR} para {MOEDA_BUY} é R${taxa_cambio}")
+        data = {"content": f"{MOEDA_VALOR} para {MOEDA_BUY} é R${taxa_cambio}"}
+        response = requests.post(WEBHOOK_URL, data=json.dumps(data), headers={"Content-Type": "application/json"})
+        if response.status_code == 204:
+            print("Mensagem enviada com sucesso!")
+        else:
+            print(f"Falha ao enviar mensagem. Status: {response.status_code}, Resposta: {response.text}")
     else:
         print(f"Informação sobre a moeda {MOEDA_BUY} não encontrada.")
 else:
